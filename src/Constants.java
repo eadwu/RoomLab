6d17b13485dbf7c9b747419f0fba4dae63914643
Edmund Wu <fangkazuto@gmail.com>
2018-11-04 16:08:35 -0500

    Reformat code

--- src/Constants.java
+++ src/Constants.java
@@ -4,10 +4,16 @@
  * @author Edmund Wu
  */
 public class Constants {
-    /** The width of the board */
+    /**
+     * The width of the board
+     */
     public static int WIDTH = 10;
-    /** The height of the board */
+
+    /**
+     * The height of the board
+     */
     public static int HEIGHT = 10;
+
     /**
      * The maximum stamina a monster has
      */
