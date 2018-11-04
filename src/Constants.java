32a61305f453dfc891f3f0f90e42a4b4f78c2b6f
Edmund Wu <fangkazuto@gmail.com>
2018-11-04 12:13:01 -0500

    Specify general game constants

---
+++ src/Constants.java
@@ -0,0 +1,12 @@
+/**
+ * Game constants.
+ *
+ * @author Edmund Wu
+ * @version 1.0.0
+ */
+public class Constants {
+    /** The width of the board */
+    public static int WIDTH = 20;
+    /** The height of the board */
+    public static int HEIGHT = 20;
+}
